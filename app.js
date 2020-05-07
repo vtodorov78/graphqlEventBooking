60d09fba04a83c5568f5f54a54b3d819eb66cf49
vtodorov78 <vvtodorov12@gmail.com>
2020-05-08 00:03:04 +0300

    added bookings, added dynamic relations, refactoring code

--- app.js
+++ app.js
@@ -1,132 +1,28 @@
 const expresss = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/user')
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
+
 
 const app = expresss();
 
 app.use(bodyParser.json());
 
 
-app.use('/graphql',graphqlHttp({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
- 
-        schema {
-          query: RootQuery
-          mutation: RootMutation
-      }
-   `),
-    rootValue: {
-        events: () => {
-          return Event.find()
-          .then(events => {
-                return events.map(event => {
-                    return { ...event._doc };
-                }) 
-            })
-            .catch(err => {
-                throw err;
-            }) 
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: '5eb120d865c8bc0e32327db0'
-            });
-            let createdEvent;
-           return event
-            .save()
-            .then(result => {
-                createdEvent = { ...result._doc };
-                return User.findById('5eb120d865c8bc0e32327db0');
-            })
-            .then(user => {
-                if (!user) {
-                throw new Error('User not found.');
-                }
-                user.createdEvents.push();
-                return user.save()
-            })
-            .then(result => {
-                return createdEvent
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            }); 
-        },
-        createUser: args => {
-         return User.findOne({email: args.userInput.email}).then(user => {
-                if (user) {
-                    throw new Error('User already exists.');
-                }
-                return bcrypt.hash(args.userInput.password, 12)
-            })  
-            .then(hashedPassword => {
-                const user = new User ({
-                    email: args.userInput.email,
-                    password: hashedPassword
-            });
-            return user.save();
-           })
-           .then(result => {
-               return { ...result._doc }
-           })
-            .catch(err => {
-                throw err;
-            })
-        }
-    },
-    graphiql: true 
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true
 }));
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-wqsu0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 ).then(() => {
     app.listen(3000);
 }).catch(err => {
-    console.log(err)    
+    console.log(err)
 });
 
 
