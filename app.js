2707cd86daa3bea5a6672885a8080ba934d26409
vtodorov78 <vvtodorov12@gmail.com>
2020-06-04 00:32:16 +0300

    added frontend, navigation, token,login,modal and backdrop

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const app = expresss();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(isAuth);
 
 
@@ -22,7 +33,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-wqsu0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 ).then(() => {
-    app.listen(3000);
+    app.listen(5000);
 }).catch(err => {
     console.log(err)
 });
